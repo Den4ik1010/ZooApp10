bdce38a9caa2665053033cf9066880dfda7c9751
renat <school@A42PC6>
2025-02-28 14:33:53 +0200

    Add project files.

---
+++ ZooApp10/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("-------WELCOME ZOO-------");
+Console.WriteLine("----------Menu-----------\r\n" +
+                 "1.Add Animal\r\n" +
+                 "2.Add feed\r\n" +
+                 "3.Edit Animal\r\n" +
+                 "4.View and manage the animal database\r\n" +
+                 "5.Management of animal fodder database\r\n" +
+                 "6.Accounting for animal feeding costs\r\n");
+public class Animals
+{
+    public string Name { get; set; }
+    public double Weight { get; set; }
+    public string View { get; set; }
+    public int Age { get; set; }
+}
+
+
