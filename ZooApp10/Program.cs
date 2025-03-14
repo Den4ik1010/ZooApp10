42e2e6993db23fe0b99af2a95a32ce7673a9fa3a
Den Geras <den4ikpig@gmail.com>
2025-03-14 14:28:36 +0200

    Hello

--- ZooApp10/Program.cs
+++ ZooApp10/Program.cs
@@ -1,17 +1,18 @@
 ﻿Console.Clear(); // очищення консолі
-Animals item = new Animals();
-
+List<Animals> items = new();    
+List<Korm> korms= new();
 while (true)
 {
     Console.Clear();
     Console.WriteLine("-------WELCOME ZOO-------");
     Console.WriteLine("----------Menu-----------\r\n" +
                      "1.Add Animal\r\n" +
-                     "2.Add feed\r\n" +
-                     "3.Edit Animal\r\n" +
+                     "2.Add korm\r\n" +
+                     "3.View Korm\r\n" +
                      "4.View and manage the animal database\r\n" +
                      "5.Management of animal fodder database\r\n" +
                      "6.Accounting for animal feeding costs\r\n");
+    Console.WriteLine("______________________________");
 
     Console.Write("Your choice: ");
     int choice = int.Parse(Console.ReadLine());
@@ -19,31 +20,79 @@ while (true)
     switch (choice)
     {
         case 1:
-            Console.WriteLine("Name");
-            item.Name = Console.ReadLine();
-            Console.WriteLine("ViewAnimals");
-            item.ViewAnimals = Console.ReadLine();
-            Console.WriteLine("Weight");
-            item.Weight = double.Parse(Console.ReadLine());
-            Console.WriteLine("Age");
-            item.Age = int.Parse(Console.ReadLine());
+            Animals newItem = new ();
+            newItem.ReadFromConsole();
+            items.Add (newItem);
             break;
+        case 2:
+            Korm newkorms= new();
+            newkorms.ReadFromConsole();
+            korms.Add (newkorms);
+            break;
+
+            case 3:
+            foreach (Korm korm in korms)
+                korm.Show();
+            break;
+            
         case 4:
-            Console.WriteLine($"Name: {item.Name}");
-            Console.WriteLine($"ViewAnimals: {item.ViewAnimals}");
-            Console.WriteLine($"Weight: {item.Weight}");
-            Console.WriteLine($"Age: {item.Age}");
+          foreach (Animals item in items)
+                item.Show();
             break;
     }
 
     Console.ReadKey();
 }
+
 public class Animals
 {
     public string Name { get; set; }
     public double Weight { get; set; }
     public string ViewAnimals { get; set; }
     public int Age { get; set; }
+
+    public void ReadFromConsole()
+    {
+        Console.Write("Enter name: ");
+        Name = Console.ReadLine();
+        Console.Write("Enter ViewAnimals: ");
+        ViewAnimals = (Console.ReadLine());
+        Console.Write(" Enter Weight: ");
+        Weight = int.Parse(Console.ReadLine());
+        Console.Write(" Age: ");
+        Age = int.Parse(Console.ReadLine());
+    }
+
+    public void Show()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"ViewAnimals: {ViewAnimals}");
+        Console.WriteLine($" Weight: {Weight}");
+        Console.WriteLine($"Age: {Age}");
+    }
 }
 
+public class Korm
+{
+    public string Name { get; set; }
+    public int Weight { get; set; }
 
+    public int Number { get; set; }
+
+    public void ReadFromConsole()
+    {
+        Console.Write("Enter Name");
+        Name = Console.ReadLine();
+        Console.Write("Enter Weight");
+        Weight = int.Parse(Console.ReadLine());
+        Console.Write("Enter Number");
+        Number = int.Parse(Console.ReadLine());
+
+    }
+    public void Show()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Weight: {Weight}");
+        Console.WriteLine($" Number: {Number}");
+    }
+}
